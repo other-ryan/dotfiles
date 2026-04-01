6e5276669395da5f2c946fe4e57cd020f11af514
Ryan Macutay <ryan.macutay@gmail.com>
2026-04-01 00:26:53 -0700

    add os to prompt

--- zsh/.config/zsh/conf.d/01-prompt.zsh
+++ zsh/.config/zsh/conf.d/01-prompt.zsh
@@ -7,7 +7,7 @@ zstyle -s ':rm:zsh:zephyr:prompt' theme ZSH_PROMPT_THEME
 zstyle ':zephyr:plugin:prompt' use-cache yes
 
 # 🚀 starship - (starship init is taken care of by zephyr)
-if [[ ${ZSH_PROMPT_USE} == starship ]]; then
+if [[ ${ZSH_PROMPT_USE:-starship} == starship ]]; then
   # VIRTUAL_ENV_DISABLE_PROMPT=1 prevents the shell from
   # print (${VENV_NAME}) at every prompt
   export VIRTUAL_ENV_DISABLE_PROMPT=1
